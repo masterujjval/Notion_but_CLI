475e94e6304c17df9185e27eee65bff991a4d979
masterujjval <guptarickey3@gmail.com>
2024-03-17 20:10:30 +0530

    changeS

--- tasks.sh
+++ tasks.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Tasks reminder and editor"
-
 i=1
-if [ $#="-d" ]
+if [ $1 == "-d"  ] 2>> /dev/null
 then
-    rm -r task.txt
-
-elif [ $# -ne 1 ];then   
-    while [ $i -ne 0 ]
+    $(rm -r task.txt)
+    echo "Tasks deleted successfully"
+elif [ $# -eq 0 ]
+then   
+    while [ 1 ]
     do
     
         echo "TASK: "
@@ -16,5 +16,9 @@ elif [ $# -ne 1 ];then
         echo "$n" >> task.txt # used echo cuz without it, the $n will be treated as a command
 
     done
+elif [ $1 == "-l" ];then
+    cat task.txt
+else
+   echo "error occured"
 fi
 
